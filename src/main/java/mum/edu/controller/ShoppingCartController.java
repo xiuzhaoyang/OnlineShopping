16d98a36fb0ebb3d206285f518c70d4fdb631209
Zhaoyang Su <suzhaoyang2004@gmail.com>
2016-10-15 15:49:25 -0500

    add simple product

---
+++ src/main/java/mum/edu/controller/ShoppingCartController.java
@@ -0,0 +1,13 @@
+package mum.edu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by su on 10/15/16.
+ */
+
+@Controller
+@RequestMapping(value="/")
+public class ShoppingCartController {
+}
